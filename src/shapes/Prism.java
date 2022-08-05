e1f7b152010d451920add23b2fcdb295f8c6ab22
Parsa Rohani Shahraki <parsa.rohanishahraki@ucalgary.ca>
2022-08-04 21:51:07 -0600

    Created prism package and Prism abstract class.

---
+++ src/shapes/Prism.java
@@ -0,0 +1,69 @@
+package shapes;
+
+public abstract class Prism {
+	// Instance Variables
+	private double height;
+	private double length;
+
+	/**
+	 * Constructor that initializes a prism object
+	 * 
+	 * @param height height of prism
+	 * @param length length of prism
+	 */
+	public Prism(double height, double length) {
+		setHeight(height);
+		setLength(length);
+	}
+
+	/**
+	 * Method to get the height of a prism
+	 * 
+	 * @return height
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/**
+	 * Method to set the height of a prism
+	 * 
+	 * @param height
+	 */
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	/**
+	 * Method to get the length of a prism
+	 * 
+	 * @return
+	 */
+	public double getLength() {
+		return length;
+	}
+
+	/**
+	 * Method to set the length of a prism
+	 * 
+	 * @param length
+	 */
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	/**
+	 * Method to calculate base area of a prism
+	 * 
+	 * @return base area
+	 */
+	public abstract double getBaseArea();
+
+	/**
+	 * Method to calculate the volume of a prism
+	 * 
+	 * @return
+	 */
+	public abstract double getVolume();
+
+}
