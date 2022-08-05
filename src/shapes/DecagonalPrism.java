7e42216fbc8d58439ec60c5002687f0e60f31cb9
Parsa Rohani Shahraki <parsa.rohanishahraki@ucalgary.ca>
2022-08-05 01:02:00 -0600

    Added decagonal prism class.

---
+++ src/shapes/DecagonalPrism.java
@@ -0,0 +1,24 @@
+package shapes;
+
+public class DecagonalPrism extends Prism {
+	/**
+	 * Contructor that initializes the Decagonal Prism
+	 * 
+	 * @param height
+	 * @param length
+	 */
+	public DecagonalPrism(double height, double length) {
+		super(height, length);
+	}
+
+	@Override
+	public double getBaseArea() {
+		return (5.0 / 2.0) * Math.pow(this.getLength(), 2) * (Math.sqrt(5 + 2 * Math.sqrt(5)));
+	}
+
+	@Override
+	public double getVolume() {
+		return ((5.0 / 2.0) * Math.pow(this.getLength(), 2) * (Math.sqrt(5 + 2 * Math.sqrt(5)))) * this.getHeight();
+	}
+
+}
