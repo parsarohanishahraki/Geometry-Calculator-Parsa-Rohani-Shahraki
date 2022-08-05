8bf18b38e4b8515d4233f5cd6843111ec6f0d594
Parsa Rohani Shahraki <parsa.rohanishahraki@ucalgary.ca>
2022-08-05 00:37:47 -0600

    Added octagonal prism class.

---
+++ src/shapes/OctagonalPrism.java
@@ -0,0 +1,23 @@
+package shapes;
+
+public class OctagonalPrism extends Prism{
+	/**
+	 * Constructor that initializes the octagonal prism
+	 * @param height
+	 * @param length
+	 */
+	public OctagonalPrism(double height, double length) {
+		super(height,length);
+	}
+
+	@Override
+	public double getBaseArea() {
+		return (2 * (1 + Math.sqrt(2)) * Math.pow(this.getLength(), 2));
+	}
+
+	@Override
+	public double getVolume() {
+		return (2 * (1 + Math.sqrt(2)) * Math.pow(this.getLength(), 2)) * this.getHeight();
+	}
+
+}
