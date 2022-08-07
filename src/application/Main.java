a53a2ca68caa2eaa416b037de3307c0e9b550b69
Parsa Rohani Shahraki <parsa.rohanishahraki@ucalgary.ca>
2022-08-07 01:10:04 -0600

    Added an fxml file, controller and created main scene for the app.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,24 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader();
+			AnchorPane root = loader.load(new FileInputStream("src/application/GeoCalcMain.fxml"));
+			Scene scene = new Scene(root,800,750);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Geometry Calculator");
---
+++ src/application/MainController.java
@@ -0,0 +1,140 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class MainController {
+	@FXML
+	private ImageView triPrism;
+	@FXML
+	private ImageView tetraPrism;
+	@FXML
+	private ImageView pentaPrism;
+	@FXML
+	private ImageView hexaPrism;
+	@FXML
+	private ImageView heptaPrism;
+	@FXML
+	private ImageView octaPrism;
+	@FXML
+	private ImageView nonaPrism;
+	@FXML
+	private ImageView decaPrism;
+	
+	@FXML
+	private void triPrismCalc() {
+		System.out.println("Heyyyy");
+		
+	}
+	@FXML
+	private void triPrismChangeHue() {
+		triPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void triPrismResetHue() {
+		triPrism.setOpacity(1);
+		
+	}
+	@FXML
+	private void tetraPrismCalc() {
+		
+	}
+	@FXML
+	private void tetraPrismChangeHue() {
+		tetraPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void tetraPrismResetHue() {
+		tetraPrism.setOpacity(1);
+		
+	}
+	@FXML
+	private void pentaPrismCalc() {
+		
+	}
+	@FXML
+	private void pentaPrismChangeHue() {
+		pentaPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void pentaPrismResetHue() {
+		pentaPrism.setOpacity(1);
+		
+	}
+	@FXML
+	private void hexaPrismCalc() {
+		
+	}
+	@FXML
+	private void hexaPrismChangeHue() {
+		hexaPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void hexaPrismResetHue() {
+		hexaPrism.setOpacity(1);
+		
+	}
+	@FXML
+	private void heptaPrismCalc() {
+		
+	}
+	@FXML
+	private void heptaPrismChangeHue() {
+		heptaPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void heptaPrismResetHue() {
+		heptaPrism.setOpacity(1);
+		
+	}
+	@FXML
+	private void octaPrismCalc() {
+		
+	}
+	@FXML
+	private void octaPrismChangeHue() {
+		octaPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void octaPrismResetHue() {
+		octaPrism.setOpacity(1);
+		
+	}
+	@FXML
+	private void nonaPrismCalc() {
+		
+	}
+	@FXML
+	private void nonaPrismChangeHue() {
+		nonaPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void nonaPrismResetHue() {
+		nonaPrism.setOpacity(1);
+		
+	}
+	@FXML
+	private void decaPrismCalc() {
+		
+	}
+	@FXML
+	private void decaPrismChangeHue() {
+		decaPrism.setOpacity(.5);
+		
+	}
+	@FXML
+	private void decaPrismResetHue() {
+		decaPrism.setOpacity(1);
+		
+	}
+	
+
+}
