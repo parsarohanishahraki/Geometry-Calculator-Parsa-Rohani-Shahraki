cbe71604d1c8746e0562942b2d34d1dd4728c940
Parsa Rohani Shahraki <parsa.rohanishahraki@ucalgary.ca>
2022-08-07 22:26:23 -0600

    Added an exception package and exception class

---
+++ src/exception/InvalidInputException.java
@@ -0,0 +1,30 @@
+package exception;
+
+public class InvalidInputException extends Exception {
+
+	public InvalidInputException() {
+		super("Invalid Input! Please enter a floating point number.");
+	}
+
+	public InvalidInputException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidInputException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
