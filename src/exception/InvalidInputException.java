435d02a3012e40a62796ec95dcc775a634502e2e
Parsa Rohani Shahraki <parsa.rohanishahraki@ucalgary.ca>
2022-08-09 13:30:30 -0600

    Fixed indentation

--- src/exception/InvalidInputException.java
+++ src/exception/InvalidInputException.java
@@ -1,30 +1,29 @@
 package exception;
 
 public class InvalidInputException extends Exception {
-
+	
+	/**
+	 * Constructor that will push the error message.
+	 */
 	public InvalidInputException() {
 		super("Please enter a floating point number!");
 	}
 
 	public InvalidInputException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidInputException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidInputException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidInputException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
